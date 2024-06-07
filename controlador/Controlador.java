68f355a511d7a29c64e64ac9c2d6d0ea1766f4fb
LauStadler <stadlerlaum@gmail.com>
2024-06-06 21:36:50 -0300

    algo de vistass

--- controlador/Controlador.java
+++ controlador/Controlador.java
@@ -3,12 +3,33 @@ package controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import models.RecursoCompartido;
+import vista.VistaCliente;
+
 public class Controlador implements ActionListener {
+
+	private RecursoCompartido modelo;
+	private VistaCliente vista;
+	
+	public Controlador(RecursoCompartido modelo, VistaCliente vista) {
+		super();
+		this.modelo = modelo;
+		this.vista = vista;
+	}
+
+
+	public RecursoCompartido getModelo() {
+		return modelo;
+	}
+
+	public VistaCliente getVista() {
+		return vista;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent evento) {
 		// TODO Auto-generated method stub
-		if(evento.getActionCommand())
+		
 	}
 
 }
